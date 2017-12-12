e7be80cff0959c5f3c59c43adf292a46f23da7e4
Prakhar Yadav <prakhary1912@gmail.com>
2017-12-12 20:43:21 +0530

    Privacy policy

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,4 +1,91 @@
 @extends('layout.master')
 
 @section('content')
+<div class="privacy">
+	<h1>Privacy Policy</h1>
+	<hr>
+	<h3>Background</h3>
+	<br>
+	<p>Top Nine 2017 understands that your privacy is important to you and that you care about how your information is used and shared online. We respect and value the privacy of everyone who visits Our Site and will only collect and use information in ways that are useful to you and in a manner consistent with your rights and Our obligations under the law.</p>
+	<br>
+	<p>This Policy applies to Our use of any and all data collected by us in relation to your use of Our Site. Please read this Privacy Policy carefully and ensure that you understand it. Your acceptance of Our Privacy Policy is deemed to occur upon your first use of Our Site.</p>
+	<br>
+	<ol style="list-style-type: decimal;">
+		<li class="bold">Definitions and Interpretation</li>
+			<p>In this Policy the following terms shall have the following meanings:</p>
+			<ul>
+				<li class="bold">Account:</li>
+					<p>means an account required to access and/or use certain areas and features of Our Site</p>
+				<li class="bold">Cookie:</li>
+					<p>means a small text file placed on your computer or device by Our Site when you visit certain parts of Our Site and when you use certain features of Our Site. Details of the Cookies used by Our Site are set out in section 12, below</p>
+				<li class="bold">Our Site:</li>
+					<p>means this website, www.TopNine2017.com</p>
+				<li class="bold">UK and EU Cookie Law:</li>
+					<p>means the relevant parts of the Privacy and Electronic Communications (EC Directive) Regulations 2003 as amended in 2004, 2011 and 2015</p>
+				<li class="bold">We/Us/Are:</li>
+					<p>means Turn Partners Limited, a limited company registered in England under 08523187, whose registered address is Faraday Wharf, Holt Street, Birmingham, B7 4BB</p>
+			</ul>
+		<li class="bold">Information About Us</li>
+			<p>Our Site, www.TopNine2017.com, is owned and operated by Turn Partners Limited, a limited company registered in England under 08523187, whose registered address is Faraday Wharf, Holt Street, Birmingham, B7 4BB.</p>
+		<li class="bold">Scope-- What Does This Policy Cover?</li>
+			<p>This Privacy Policy applies only to your use of Our Site. It does not extend to any websites that are linked to from Our Site (whether We provide those links or whether they are shared by other users). We have no control over how your data is collected, stored or used by other websites and We advise you to check the privacy policies of any such websites before providing any data to them.</p>
+		<li class="bold">What Data Do We Collect?</li>
+			<p>Some data will be collected automatically by Our Site other data will only be collected if you voluntarily submit it, for example, when signing up for an Account. Depending upon your use of Our Site, We may collect some or all of the following data:</p>
+			<ul>
+				<li>IP address (automatically collected);</li>
+				<li>web browser type and version (automatically collected);</li>
+				<li>operating system (automatically collected);</li>
+				<li>a list of URLs starting with a referring site, your activity on Our Site, and the site you exit to (automatically collected);</li>
+			</ul>
+		<li class="bold">How Do We Use Your Data?</li>
+			<ul>
+				<li>All personal data is stored securely in accordance with the principles of the Data Protection Act 1998. For more details on security see section 6, below.</li>
+				<li>We use your data to provide the best possible services to you. This includes:</li>
+					<ul>
+						<li>Providing and managing your access to Our Site;</li>
+						<li>Personalising and tailoring your experience on Our Site;</li>
+						<li>Supplying Our services to you;</li>
+						<li>Personalising and tailoring Our services for you;</li>
+						<li>Responding to communications from you;</li>
+						<li>Analysing your use of Our Site and gathering feedback to enable Us to continually improve Our Site and your user experience;</li>
+					</ul>
+				<li>With your permission and/or where permitted by law, We may also use your data for marketing purposes. We will not, however, send you any unsolicited marketing or spam and will take all reasonable steps to ensure that We fully protect your rights and comply with Our obligations under the Data Protection Act 1998 and the Privacy and Electronic Communications (EC Directive) Regulations 2003, as amended in 2004, 2011 and 2015.</li>
+			</ul>
+		<li class="bold">How and Where Do We Store Your Data?</li>
+			<ul>
+				<li>We only keep your data for as long as We need to in order to use it as described above in section 5, and/or for as long as We have your permission to keep it.</li>
+				<li>Some or all of your data may be stored or transferred outside of the European Economic Area (“the EEA”) (The EEA consists of all EU member states, plus Norway, Iceland and Liechtenstein). You are deemed to accept and agree to this by using Our Site and submitting information to Us. If We do store or transfer data outside the EEA, We will take all reasonable steps to ensure that your data is treated as safely and securely as it would be within the EEA and under the Data Protection Act 1998. Such steps may include, but not be limited to, the use of legally binding contractual terms between Us and any third parties We engage and the use of the EU-approved Model Contractual Arrangements.</li>
+			</ul>
+		<li class="bold">Do We Share Your Data?</li>
+			<ul>
+				<li>We may share your data with other companies in Our group. This includes Our holding company and its subsidiaries.</li>
+				<li>We may sometimes contract with third parties to supply products and services to you on Our behalf. These may include payment processing, delivery of goods, search engine facilities, advertising and marketing. In some cases, the third parties may require access to some or all of your data. Where any of your data is required for such a purpose, We will take all reasonable steps to ensure that your data will be handled safely, securely, and in accordance with your rights, Our obligations, and the obligations of the third party under the law.</li>
+				<li>We may compile statistics about the use of Our Site including data on traffic, usage patterns, user numbers, sales and other information. All such data will be anonymised and will not include any personally identifying information. We may from time to time share such data with third parties such as prospective investors, affiliates, partners and advertisers. Data will only be shared and used within the bounds of the law.</li>
+				<li>In certain circumstances We may be legally required to share certain data held by Us, which may include your personal information, for example, where We are involved in legal proceedings, where We are complying with the requirements of legislation, a court order, or a governmental authority. We do not require any further consent from you in order to share your data in such circumstances and will comply as required with any legally binding request that is made of Us.</li>
+			</ul>
+		<li class="bold">What Happens If Our Business Changes Hands?</li>
+			<ul>
+				<li>We may, from time to time, expand or reduce Our business and this may involve the sale and/or the transfer of control of all or part of Our business. Data provided by users will, where it is relevant to any part of Our business so transferred, be transferred along with that part and the new owner or newly controlling party will, under the terms of this Privacy Policy, be permitted to use the data for the purposes for which it was originally collected by Us.</li>
+				<li>In the event that any of your data is to be transferred in such a manner, you will not be contacted in advance and informed of the changes. When contacted you will not, however, be given the choice to have your data deleted or withheld from the new owner or controller.</li>
+			</ul>
+		<li class="bold">How Can You Control Your Data?</li>
+			<ul>
+				<li>When you submit information via Our Site, you may be given options to restrict Our use of your data. In particular, We aim to give you strong controls on Our  use of your data for direct marketing purposes (including the ability to opt-out of receiving emails from Us which you may do by unsubscribing using the links provided in Our emails.</li>
+				<li>You may also wish to sign up to one or more of the preference services operating in the UK: The Telephone Preference Service (“the TPS”), the Corporate Telephone Preference Service (“the CTPS”), and the Mailing Preference Service (“the MPS”). These may help to prevent you receiving unsolicited marketing. Please note, however, that these services will not prevent you from receiving marketing communications that you have consented to receiving.</li>
+			</ul>
+		<li class="bold">Your Right to Withhold Information</li>
+			<ul>
+				<li>You may access  Our Site without providing any data at all. However, to use all features and functions available on Our Site you may be required to submit or allow for the collection of certain data.</li>
+				<li>You may restrict your internet browser’s use of Cookies. For more information, see section 12.</li>
+			</ul>
+		<li class="bold">How Can You Access Your Data?</li>
+			<p>You have the legal right to ask for a copy of any of your personal data held by Us (where such data is held) on payment of a small fee which will not exceed £15. Please contact Us for more details at jobs@turnpartners.com, or using the contact details below in section 13.</p>
+		<li class="bold">Cookies</li>
+			<p>Our Site may place and access certain first party Cookies on your computer or device. First party Cookies are those placed directly by Us and are used only by Us. We use Cookies to facilitate and improve your experience of Our Site and to provide and improve Our services.  Our Site also uses analytics services provided by Google Analytics, which also use Cookies. Website analytics refers to a set of tools used to collect and analyse usage statistics, enabling Us to better understand how people use Our Site. </p>
+		<li class="bold">Contacting Us</li>
+			<p>If you have any questions about Our Site or this Privacy Policy, please contact Us by email at jobs@turnpartners.com, by telephone on (0044) 8008128085, or by post at Faraday Wharf, Holt Street, Birmingham, B7 4BB, England. Please ensure that your query is clear, particularly if it is a request for information about the data We hold about you (as under section 11, above).</p>
+		<li class="bold">Changes to Our Privacy Policy</li>
+			<p>We may change this Privacy Policy as we may deem necessary from time to time, or as may be required by law. Any changes will be immediately posted on Our Site and you will be deemed to have accepted the terms of the Privacy Policy on your first use of Our Site following the alterations. We recommend that you check this page regularly to keep up-to-date.</p>
+	</ol>
+</div>
 @endsection
